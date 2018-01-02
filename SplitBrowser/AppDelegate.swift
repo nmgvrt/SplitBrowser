4d99ded2399e4158e6d1099e8b98c367cfa3d3d1
Tada Ryunosuke <tada.ryunosuke@ppl.cs.tut.ac.jp>
2018-01-02 10:31:23 +0900

    Initial Commit

---
+++ SplitBrowser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SplitBrowser
+//
+//  Created by ryu on 2018/01/02.
+//  Copyright © 2018年 Ryunosuke Tada. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SplitBrowser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SplitBrowser
+//
+//  Created by ryu on 2018/01/02.
+//  Copyright © 2018年 Ryunosuke Tada. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
